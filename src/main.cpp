d33e89104f4d3fb5664a6674945cbe957d0ff5a9
lvalerom <lvaleromartin@gmail.com>
2016-08-13 20:08:59 +0200

    date

--- src/main.cpp
+++ src/main.cpp
@@ -13,26 +13,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Through the galaxy.  If not, see <http://www.gnu.org/licenses/>.
 */
-#ifdef _DEBUG
-
-#include <sstream>
-#include <time.h>
-
-#define _VISUALLEAKDETECTOR
-
-#ifdef _VISUALLEAKDETECTOR
-//#include <vld.h>
-#endif
-
-#if (!defined _VISUALLEAKDETECTOR) && (defined _LEAKDETECTOR) && (defined _WIN32)
-#include <crtdbg.h>
-#include <assert.h>
-#endif 
-
-#endif
-
-#include <csignal>
-#include <string>
 
 #include <common/log/log.h>
 
@@ -40,10 +20,6 @@ along with Through the galaxy.  If not, see <http://www.gnu.org/licenses/>.
 #if (defined _DEBUG) || !(defined _WIN32)
 int main(int argc, char **argv)
 {
-#if (!defined _VISUALLEAKDETECTOR) && (defined _LEAKDETECTOR)
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-	_CrtSetBreakAlloc(-1);
-#endif
 #else
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
